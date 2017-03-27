8b12cd286f4f98f025e3646a37c020fa71222d41
hosi <ansekal456@gmail.com>
2017-03-27 09:43:26 +0900

    ver01

---
+++ src/test/AppMain.java
@@ -0,0 +1,11 @@
+package test;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		System.out.println(Integer.toString(a));
+	}
+
+}
